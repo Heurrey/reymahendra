000843f98323c85257d28501cbadd248281eae49
heurrey <reymhndra10@gmail.com>
2023-09-23 14:25:40 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,js}'],
+  theme: {
+    container: {
+      center: true,
+      padding: '16px'
+    },
+    extend: {
+      colors: {
+        white: '#fafaee',
+        blue: '#1820EF',
+        red: '#FF4F42',
+        yellow: '#FFC023',
+      },
+      screen: {
+        '2xl': '1320px',
+      },
+      fontFamily: {
+        'sans': ['Poppins', 'sans-serif'],
+      },
+    },
+    backgroundImage: {
+      'her': "url('dist/img/reyMhndra1.png')",
+    }
+  },
+  plugins: [],
+}
+
+
+
+
+
